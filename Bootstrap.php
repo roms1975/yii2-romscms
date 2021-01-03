b7388f98c66b8afdeb518691f7462ee70ae952d7
roms1975 <odggbo@gmail.com>
2021-01-03 11:58:27 +0300

    dd

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace roms\cms;
 use Yii;
 use yii\base\BootstrapInterface;
+
 class Bootstrap implements BootstrapInterface{
     //Метод, который вызывается автоматически при каждом запросе
     public function bootstrap($app)
